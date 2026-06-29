bc1008353f269c43bf1575262327a1e22734d783
pruchafil <prucha fil@gmail.com>
2026-06-30 00:40:57 +0200

    main, psql integration

---
+++ infrastructure/Database/Connection.php
@@ -0,0 +1,15 @@
+<?php
+
+function getConnection(): PDO {
+    $host = $_ENV['DB_HOST'];
+    $db = $_ENV['DB_DB'];
+    $user = $_ENV['DB_USER'];
+    $pass = $_ENV['DB_PASS'];
+    $dsn = "pgsql:host=$host;dbname=$db";
+
+    try {
+        return new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    } catch (PDOException $e) {
+        exit( "Connection failed: " . $e->getMessage());
+    }
+}
---
+++ main.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+require_once __DIR__ . '/infrastructure/Database/Connection.php';
+
+getConnection();
